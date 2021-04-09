e48bbc6296709ee8e37c7a4f84a90f0bca4ec219
Raúl <raulguzmanguerrero@outlook.com>
2021-04-08 23:42:36 -0500

    Add ShowMessages

--- cliente/static/index.js
+++ cliente/static/index.js
@@ -9,6 +9,10 @@ class Contacto{
         // El elemento en el DOM que representará al contacto
         this.DOMElement = this.CreateDOMElement();
         this.ws = null;
+        this.messages = [
+            {type: 'enviado', content: '¡Hola!'},
+            {type: 'recibido', content: 'Adios.'}
+        ]
     };
 
     CreateDOMElement(){
@@ -35,13 +39,6 @@ class Contacto{
         button.appendChild(profile_picture);
         button.appendChild(name);
 
-        // Mostrar la ventana de chat, y si no existe, crear el WS que le corresponde al contacto
-        button.addEventListener('click', () => {
-            if(this.ws == null){
-                this.ws = this.CreateWebSocket();
-            }
-        });
-
         return button;
     }
 
@@ -56,13 +53,72 @@ class Contacto{
     }
 }
 
+// Muestra los mensajes almacenados del contacto en la caja de mensajes
+const ShowMessages = (mensajes) => {
+    let contenedor_mensajes = document.querySelector('#mensajes');
+    
+    // Eliminar los elementos que representan los mensajes enviados y recibidos que se encuentren actualmente en la caja de mensajes
+    contenedor_mensajes.innerHTML = "";
+    
+    mensajes.forEach((mensaje) => {
+        let contenedor_mensaje = document.createElement('div');
+        let contenido_mensaje = document.createElement('div');
+
+
+        switch(mensaje.type){
+            case 'recibido':
+                let profile_picture = document.createElement('img');
+
+                // Definición de profile_picture
+                profile_picture.src = 'img/user.png';
+                profile_picture.alt = 'User';
+                profile_picture.classList.add('user');
+
+                contenedor_mensaje.appendChild(profile_picture);
+
+                // Definición de mensaje
+                contenido_mensaje.classList.add('box', 'sb2');
+                contenido_mensaje.innerText = mensaje.content;
+
+                // Definición de contenedor_mensaje
+                contenedor_mensaje.classList.add('receive', 'd-flex', 'col-sm-12', 'align-items-center', 'justify-content-start');
+
+                break;
+            case 'enviado':
+                // Definición de mensaje
+                contenido_mensaje.classList.add('box', 'sb1');
+                contenido_mensaje.innerText = mensaje.content;
+
+                // Definición de contenedor_mensaje
+                contenedor_mensaje.classList.add('sending', 'd-flex', 'col-sm-12', 'align-items-center', 'justify-content-end');
+                
+                break;
+            default:
+                break;
+        }
+
+        contenedor_mensaje.appendChild(contenido_mensaje);
+        contenedor_mensajes.appendChild(contenedor_mensaje);
+    });
+};
+
+let currentContact = null;
 let contactos = [];
 
 window.onload = () => {
     // Por el momento solo existirá un solo contacto, en el futuro, las personas conectadas se obtendrán 
     // del servidor
     let nuevoContacto = new Contacto();
-    contactos.push(nuevoContacto);
+    contactos.push(nuevoContacto);   
+
+    // Mostrar la ventana de chat, y si no existe, crear el WS que le corresponde al contacto
+    nuevoContacto.DOMElement.addEventListener('click', () => {
+        if(nuevoContacto.ws == null){
+            nuevoContacto.ws = nuevoContacto.CreateWebSocket();
+        }
+
+        ShowMessages(nuevoContacto.messages);
+    });
 
     document.querySelector('.botones').appendChild(nuevoContacto.DOMElement);
 }
