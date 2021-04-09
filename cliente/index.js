7df973b4fc5d3af88ea98a1b3b7f58e22e81de43
Raúl <raulguzmanguerrero@outlook.com>
2021-04-09 18:52:32 -0500

    Send IPC Message when receiving message from other client

--- cliente/index.js
+++ cliente/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const io = require("socket.io");
 const http = require("http");
 
-const {app, BrowserWindow, contextBridge} = electron;
+const {app, BrowserWindow, ipcMain} = electron;
 const server = http.createServer();
 const socket_server = io(server, {
     // Este puerto será definido para cada cliente durante el proceso de inicio de sesión. Por el momento se dejará como 3000
@@ -17,7 +17,11 @@ let index;
 // Listen for app to be ready
 app.on('ready', function() {
     // Create new window
-    index = new BrowserWindow({});
+    index = new BrowserWindow({
+        webPreferences: {
+            preload: app.getAppPath() + '/preload.js'
+        }
+    });
     // Load HTML into window
     index.loadURL(url.format({
         pathname: path.join(__dirname, 'login.html'),
@@ -29,8 +33,14 @@ app.on('ready', function() {
 // Esta sección controla todo lo que tenga que ver con los WebSockets
 socket_server.on('connection', (socket) => {
     console.log("Nueva conexión detectada");
+
+    // Cuando se recibe un mensaje, enviarlo al proceso Render
     socket.on('message', (message) => {
         console.log("Se ha recibido un mensaje, enviandolo al proceso Render...");
+
+        message.origin = 'received';
+
+        index.webContents.send('message', message);
     });
 });
 
